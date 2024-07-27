e4d3b550390e09cc80765cd4b7a3dde3432b0760
Taharka2000 <sammbasow1999@gmail.com>
2024-07-27 16:57:00 +0000

    Initial commit of React project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> Hello world </h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt velit, consectetur excepturi nulla quia labore odit minima possimus voluptas. Dolor rerum itaque vitae perspiciatis commodi distinctio doloremque voluptas id! Maxime!</p>
     </div>
   );
 }
