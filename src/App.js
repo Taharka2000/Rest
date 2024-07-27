e2d65db2c283bd4d6ad94a6b1ccce4a8a29fee4f
Taharka2000 <sammbasow1999@gmail.com>
2024-07-27 21:55:39 +0000

    Mise à jour du projet React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1> Hello world </h1>
       <h2>hello</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt velit, consectetur excepturi nulla quia labore odit minima possimus voluptas. Dolor rerum itaque vitae perspiciatis commodi distinctio doloremque voluptas id! Maxime!</p>
+      <p>Lorem ipsum dolor sit amet consecnsectetur exceptur rerum itaque vitae perspiciatis commodi distinctio doloremque voluptas id! Maxime!</p>
     </div>
   );
 }
